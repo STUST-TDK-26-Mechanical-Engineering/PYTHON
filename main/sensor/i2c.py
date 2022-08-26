50c32448ceb726466e2afc5b91f099c8622bebac
LWCJacky <54478652+LWCJacky@users.noreply.github.com>
2022-08-26 20:13:35 +0800

    v2.6.1

---
+++ main/sensor/i2c.py
@@ -0,0 +1,21 @@
+from smbus import *
+import time
+ARDUINO_ADDR = 0x42
+I2C_BUS_NO = 8
+
+i2c_bus = SMBus(I2C_BUS_NO)
+
+#while 1:
+   # try:
+    #    message = input("Message to be send: ")
+   # except:
+  #      break
+ #   for a in [ord(c) for c in message]:
+#        i2c_bus.write_byte(ARDUINO_ADDR, a)
+while 1:
+    #bus.pec = 1  # Enable PEC
+    time.sleep(0.5)
+    # i2c_bus.write_byte(ARDUINO_ADDR, a)
+    i2c_bus.write_i2c_block_data(ARDUINO_ADDR, 0x00, "a")
+    b = i2c_bus.read_byte_data(ARDUINO_ADDR, 1)
+    print(b)
