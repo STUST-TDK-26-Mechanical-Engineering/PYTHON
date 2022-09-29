2e56910aa41bf746099542222d35c51732ede484
LWCJacky <54478652+LWCJacky@users.noreply.github.com>
2022-09-29 21:05:34 +0800

    v2.7.4

---
+++ arduino/trace/trace.ino
@@ -0,0 +1,125 @@
+#include <AutoPID.h>
+#include <Wire.h>
+#define  I2C_ADDRESS 0x28 //主機位置
+#define DO1 5
+#define DO2 4
+#define DO3 3
+#define DO4 2
+#define RDO1 6  
+#define RDO2 7
+#define RDO3 8
+#define RDO4 9
+
+#define OUTPUT_MIN -255
+#define OUTPUT_MAX 255
+#define KP 1
+#define KI 0.003
+#define KD 0
+uint8_t opcode; // register
+uint8_t speed;  // fan speed: 0=off, 150=low, 200=medium, 250=high
+double input, setPoint, outputVal;
+AutoPID myPID(&input, &setPoint, &outputVal, OUTPUT_MIN, OUTPUT_MAX, KP, KI, KD);
+void init_i(){
+  pinMode(DO1,INPUT);
+  pinMode(DO2,INPUT);
+  pinMode(DO3,INPUT);
+  pinMode(DO4,INPUT);
+  pinMode(RDO1,INPUT);
+  pinMode(RDO2,INPUT);
+  pinMode(RDO3,INPUT);
+  pinMode(RDO4,INPUT);
+}
+void setup() {
+  Serial.begin(9600);                       
+  init_i();
+  // myPID.setBangBang(4);
+  myPID.setTimeStep(400);
+  Wire.begin(I2C_ADDRESS);
+  Wire.onRequest(requestEvent);
+  Wire.onReceive(receiveEvent);
+  setPoint=0;  
+} 
+int pid_val=0;
+void loop() {
+  int val = 500;
+  int low = 0x00ff & val;
+  int high = (0xff00 & val) >> 8;
+  Serial.print(String(val, HEX));
+  Serial.print("\t");
+  Serial.print(String(low, HEX));
+  Serial.print("\t");
+  Serial.print(String(high, HEX));
+  Serial.print("\t");
+  // String(-500, HEX);
+  myPID.run(); //call every loop, updates automatically at certain time interval
+  if (!digitalRead(DO2)){
+    input+=1;
+  }
+  if (!digitalRead(DO1)){
+    input+=10;
+  }
+  if (!digitalRead(DO3)){
+    input-=1;
+  }if (!digitalRead(DO4)){
+    input-=10;
+  }
+  if(!digitalRead(DO2)&&!digitalRead(DO3)){
+    // myPID.reset();
+    input=0;
+    outputVal=0;
+  }
+  Serial.print(outputVal);
+  Serial.print("\t");
+  Serial.print(input);
+  Serial.print("\t");
+  Serial.print(digitalRead(DO1));
+  Serial.print("\t");
+  Serial.print(digitalRead(DO2));
+  Serial.print("\t");
+  Serial.print(digitalRead(DO3));
+  Serial.print("\t");
+  Serial.print(digitalRead(DO4));
+  Serial.println("\t");
+  // Serial.print(digitalRead(RDO1));
+  // Serial.print("\t");
+  // Serial.print(digitalRead(RDO2));
+  // Serial.print("\t");
+  // Serial.print(digitalRead(RDO3));
+  // Serial.print("\t");
+  // Serial.print(digitalRead(RDO4));
+  // Serial.print("\n");
+  // delay(500);
+}
+void receiveEvent(int bytes) {
+  //讀取第一個字節以確定涉及哪個寄存器
+  opcode = Wire.read();
+  // Serial.println(opcode);
+  // 如果超過 1 個字節，則主機正在寫入從機
+  if (bytes > 1) {
+    // if (opcode == REGISTER_SPEED) {
+    //   speed = Wire.read();
+    //   Serial.println(speed);
+    //   Serial.println(opcode);
+    // } else if (opcode == REGISTER_POWER) {
+    //   speed = (Wire.read() == 1) ? 200 /* on */ : 0 /* off */;
+    // }
+  }
+}
+byte data[4];
+void requestEvent() {
+  // Read from the register variable to know what to send back
+  Serial.print("Event");
+  int val = 500;
+  int low = 0x00ff & val;
+  int high = (0xff00 & val) >> 8;
+  data[0]=high;  
+  data[0]=low;
+  // Serial.print(sizeof(speed));
+  if (opcode == 0x01) {
+    for (int i = 0; i < 4; i++) data[i] = Wire.read();
+    // Wire.write((uint8_t *)&speed, sizeof(speed));
+  } else {
+    delay(5000);
+    // Wire.write(3);
+  }
+}
---
+++ main/tracking_sensor/i2c.py
@@ -0,0 +1,34 @@
+from smbus import *
+import time
+ARDUINO_ADDR = 0x42
+I2C_BUS_NO = 8
+
+
+
+#while 1:
+   # try:
+    #    message = input("Message to be send: ")
+   # except:
+  #      break
+ #   for a in [ord(c) for c in message]:
+#        i2c_bus.write_byte(ARDUINO_ADDR, a)
+class master:
+    def __init__(self) -> None:
+        self.ARDUINO_ADDR = 0x50
+        self.I2C_BUS_NO = 1
+        self.i2c_bus = SMBus(I2C_BUS_NO)
+    def send_test(self,mode,data=0):
+
+        try:
+            #bus.pec = 1  # Enable PEC
+            time.sleep(0.5)
+            # i2c_bus.write_byte(ARDUINO_ADDR, a)
+            self.i2c_bus.write_i2c_block_data(self.ARDUINO_ADDR,mode,[data])
+            # print("1")
+            b = self.i2c_bus.read_byte_data(self.ARDUINO_ADDR,0x01)
+            print(b)
+        except OSError:
+            print("OSError")    
+
+a=master()
+a.send_test(0x23)
