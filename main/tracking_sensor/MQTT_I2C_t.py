e1c25d5bf209a82286a5c78ec3a3291211fe8e06
LWCJacky <54478652+LWCJacky@users.noreply.github.com>
2022-10-02 05:20:28 +0800

    v2.7.15

--- main/tracking_sensor/MQTT_I2C_t.py
+++ main/tracking_sensor/MQTT_I2C_t.py
@@ -1,5 +1,5 @@
 from paho.mqtt import client as mqtt_client
-from i2c import master
+# from i2c import master
 import random
 import time
 import threading
@@ -8,7 +8,7 @@ broker = 'r201_nx.local'
 port = 1883
 topic = "/bot/sensor/tracking"
 client_id = f'sensor-{random.randint(0, 1000)}'
-i2c=master()
+# i2c=master()
 def connect_mqtt():#連接伺服器
     def on_connect(client, userdata, flags, rc):
         if rc == 0:
@@ -21,31 +21,25 @@ def connect_mqtt():#連接伺服器
     client.connect(broker, port)
     return client
 def text(client: mqtt_client):
-    client.publish(topic="/bot/chassis", payload={
-         "msg": "log.play",
-         "X": -100,
-         "Y": 0,
-         "Z": 100,
-         "S": 1
-      }, qos=0)
+    client.publish(topic="/bot/chassis", payload=json.dumps({"msg": "log.play","X": -100,"Y": 0,"Z": 0,"S": 1}), qos=0)
 def subscribe(client: mqtt_client):#訂閱
     def on_message(client, userdata, msg):
         print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
         m_in=json.loads(msg.payload.decode()) #decode json data
         print(m_in["msg"])
         
-        if m_in["msg"]=="i2c.test":
-            mode=m_in["mode"] 
-            if mode == "0x01":
-                i2c.send_test(0x01)
-            elif mode=="0x02":
-                i2c.send_test(0x02)  
-            elif mode=="0x03":
-                i2c.send_test(0x03,m_in["data"]) 
-            elif mode=="0x04":
-                i2c.send_test(0x04,m_in["data"])
-            elif mode=="0x05":
-                i2c.send_test(0x05)          
+        # if m_in["msg"]=="i2c.test":
+        #     mode=m_in["mode"] 
+        #     if mode == "0x01":
+        #         i2c.send_test(0x01)
+        #     elif mode=="0x02":
+        #         i2c.send_test(0x02)  
+        #     elif mode=="0x03":
+        #         i2c.send_test(0x03,m_in["data"]) 
+        #     elif mode=="0x04":
+        #         i2c.send_test(0x04,m_in["data"])
+        #     elif mode=="0x05":
+        #         i2c.send_test(0x05)          
             
     client.subscribe(topic)
     client.on_message = on_message
