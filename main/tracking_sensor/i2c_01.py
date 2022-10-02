fee132e54d809c5df6b8ac07b707eed6fd57cab5
LWCJacky <54478652+LWCJacky@users.noreply.github.com>
2022-10-02 23:15:27 +0800

    Update i2c_01.py

--- main/tracking_sensor/i2c_01.py
+++ main/tracking_sensor/i2c_01.py
@@ -19,7 +19,9 @@ class master:
         self.i2c_bus = SMBus(self.I2C_BUS_NO)
     def res(self):
         try:
-            self.i2c_bus.write_i2c_block_data(self.ARDUINO_ADDR,0xA4,[0x01])    
+            self.i2c_bus.write_i2c_block_data(self.ARDUINO_ADDR,0xA4,[0x01])
+            print("res")
+            time.sleep(1)    
         except:
             print("i2c錯誤")    
     def send_test(self,mode=0xff,data=0):
