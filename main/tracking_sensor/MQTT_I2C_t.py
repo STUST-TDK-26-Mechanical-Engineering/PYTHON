9162fcf64cbbf71e5849ea0be963266fca42d16a
LWCJacky <54478652+LWCJacky@users.noreply.github.com>
2022-10-02 19:54:34 +0800

    Update MQTT_I2C_t.py

--- main/tracking_sensor/MQTT_I2C_t.py
+++ main/tracking_sensor/MQTT_I2C_t.py
@@ -24,7 +24,7 @@ def text(client: mqtt_client):
     while 1:
 
         data=i2c.send_test(0x01)
-        client.publish(topic="/bot/chassis", payload=json.dumps({"msg": "log.play","X": 0,"Y": data,"Z": 0,"S": 1}), qos=0)
+        client.publish(topic="/bot/chassis", payload=json.dumps({"msg": "log.play","X": 0,"Y": data,"Z": 0,"S": 0.5}), qos=0)
 def subscribe(client: mqtt_client):#訂閱
     def on_message(client, userdata, msg):
         print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
