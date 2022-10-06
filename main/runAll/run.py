59dc46ac000b64ffe8b447006af9a85ca5737d14
LWCJacky <54478652+LWCJacky@users.noreply.github.com>
2022-10-07 00:27:19 +0800

    Update run.py

--- main/runAll/run.py
+++ main/runAll/run.py
@@ -2,34 +2,93 @@ import subprocess,time,sys
 
 TIME = 10                        #程序状态检测间隔（单位：分钟）
 # CMD = "/home/r201/Documents/PYTHON/main/Order_record/log.py"#需要执行程序的绝对路径，支持jar 如：D:\\calc.exe 或者D:\\test.jar
-CMD ="/home/r201/Documents/PYTHON/main/chassis_movement/MQTT.py"
-
-
+chassis_movement ="/home/r201/Documents/PYTHON/main/chassis_movement/MQTT.py"
+log="/home/r201/Documents/PYTHON/main/Order_record/log.py"
+tracking_sensor="/home/r201/Documents/PYTHON/main/tracking_sensor/MQTT_I2C_t.py"
+sensor="/home/r201/Documents/PYTHON/main/sensor/MQTT_I2C.py"
 class Auto_Run():
-    def __init__(self,sleep_time,cmd):
+    def __init__(self,sleep_time,chassis_movement,log,tracking_sensor,sensor):
         self.sleep_time = sleep_time
-        self.cmd = cmd
-        self.ext = (cmd[-3:]).lower()        #判断文件的后缀名，全部换成小写
-        self.p = None                        #self.p为subprocess.Popen()的返回值，初始化为None
-        self.run()                           #启动时先执行一次程序
+        self.chassis_movement = chassis_movement
+        self.log=log
+        self.tracking_sensor=tracking_sensor
+        self.sensor=sensor
+
+        self.chassis_ext = (chassis_movement[-3:]).lower()        #判断文件的后缀名，全部换成小写
+        self.log_ext = (log[-3:]).lower()
+        self.tracking_sensor_ext = (tracking_sensor[-3:]).lower()
+        self.sensor_ext = (sensor[-3:]).lower()
+
+        self.chassis_movement_p = None                        #self.p为subprocess.Popen()的返回值，初始化为None
+        self.log_p = None
+        self.tracking_sensor_p = None
+        self.sensor_p = None
+        # self.p = None
+        self.chassis_movement_run() 
+        self.log_run()
+        self.tracking_sensor_run()
+        self.sensor_run()
+        # self.run()                          #启动时先执行一次程序
 
         try:
             while 1:
                 time.sleep(sleep_time )  #休息10分钟，判断程序状态
-                self.poll = self.p.poll()    #判断程序进程是否存在，None：表示程序正在运行 其他值：表示程序已退出
-                if self.poll is None:
-                    print ("运行正常")
+                # self.poll = self.p.poll()    #判断程序进程是否存在，None：表示程序正在运行 其他值：表示程序已退出
+                if self.chassis_movement_p.poll() is None:
+                    print ("chassis_movement正常")
                 else:
-                    print ("未检测到程序运行状态，准备启动程序")
+                    print ("chassis_movement 未正常運作 重起中...")
+                    self.chassis_movement_run()
+
+                if self.log.poll() is None:
+                    print ("log正常")
+                else:
+                    print ("log 未正常運作 重起中...")
                     self.run()
+                if self.tracking_sensor.poll() is None:
+                    print ("正常")
+                else:
+                    print ("tracking_sensor 未正常運作 重起中...")
+                    self.tracking_sensor_run()
+                if self.sensor.poll() is None:
+                    print ("sensor 正常")
+                else:
+                    print ("sensor 未正常運作 重起中...")
+                    self.run()                
         except KeyboardInterrupt as e:
             print ("检测到CTRL+C,准备退出程序!")
 #            self.p.kill()                   #检测到CTRL+C时，kill掉CMD中启动的exe或者jar程序
 
-    def run(self):
-        if self.ext == ".py":
-            print ('start OK!')
-            self.p = subprocess.Popen(['sudo','python3','%s' % self.cmd], stdin = sys.stdin,stdout = sys.stdout, stderr = sys.stderr, shell = False)
+    def chassis_movement_run(self):
+        if self.chassis_ext == ".py":
+            print ('chassis_movement_start OK!')
+            self.chassis_movement_p = subprocess.Popen(['sudo','python3','%s' % self.chassis_movement], stdin = sys.stdin,stdout = sys.stdout, stderr = sys.stderr, shell = False)
         else:
             pass
-app = Auto_Run(TIME,CMD)
+    def log_run(self):
+        if self.log_ext == ".py":
+            print ('log_start OK!')
+            self.log_p = subprocess.Popen(['python3','%s' % self.log], stdin = sys.stdin,stdout = sys.stdout, stderr = sys.stderr, shell = False)
+        else:
+            pass
+    def tracking_sensor_run(self):
+        if self.tracking_sensor_ext == ".py":
+            print ('tracking_sensor_start OK!')
+            self.tracking_sensor_p = subprocess.Popen(['sudo','python3','%s' % self.cmd], stdin = sys.stdin,stdout = sys.stdout, stderr = sys.stderr, shell = False)
+        else:
+            pass
+    def sensor_run(self):
+        if self.sensor_ext == ".py":
+            print ('sensor_run_start OK!')
+            self.sensor_p = subprocess.Popen(['sudo','python3','%s' % self.cmd], stdin = sys.stdin,stdout = sys.stdout, stderr = sys.stderr, shell = False)
+        else:
+            pass
+    # def run(self):
+    #     if self.ext == ".py":
+    #         print ('start OK!')
+    #         self.p = subprocess.Popen(['sudo','python3','%s' % self.cmd], stdin = sys.stdin,stdout = sys.stdout, stderr = sys.stderr, shell = False)
+    #     else:
+    #         pass                
+app = Auto_Run(sleep_time=TIME,chassis_movement=chassis_movement,
+                log=log,tracking_sensor=tracking_sensor,
+                sensor=sensor)
