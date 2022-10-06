67e0d0bddedf757b56bfeab25db170918fcef73b
LWCJacky <54478652+LWCJacky@users.noreply.github.com>
2022-10-06 23:37:03 +0800

    Update run.py

--- main/runAll/run.py
+++ main/runAll/run.py
@@ -29,7 +29,7 @@ class Auto_Run():
     def run(self):
         if self.ext == ".py":
             print ('start OK!')
-            self.p = subprocess.Popen(['python','%s' % self.cmd], stdin = sys.stdin,stdout = sys.stdout, stderr = sys.stderr, shell = False)
+            self.p = subprocess.Popen(['python3','%s' % self.cmd], stdin = sys.stdin,stdout = sys.stdout, stderr = sys.stderr, shell = False)
         else:
             pass
 app = Auto_Run(TIME,CMD)
