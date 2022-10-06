5419918cc4ecdb0c64b69b190086cc28a1d7367a
LWCJacky <54478652+LWCJacky@users.noreply.github.com>
2022-10-06 23:36:10 +0800

    Update run.py

--- main/runAll/run.py
+++ main/runAll/run.py
@@ -1,7 +1,7 @@
 import subprocess,time,sys
 
 TIME = 10                        #程序状态检测间隔（单位：分钟）
-CMD = "~/Documents/PYTHON/main/Order_record/log.py"#需要执行程序的绝对路径，支持jar 如：D:\\calc.exe 或者D:\\test.jar
+CMD = "/home/r201/Documents/PYTHON/main/Order_record/log.py"#需要执行程序的绝对路径，支持jar 如：D:\\calc.exe 或者D:\\test.jar
 
 
 
