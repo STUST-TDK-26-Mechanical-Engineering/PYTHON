b7dfad2edaa5b4e81e0227ae625ac3035c407d67
LWCJacky <54478652+LWCJacky@users.noreply.github.com>
2022-10-07 01:51:35 +0800

    Update run.py

--- main/runAll/run.py
+++ main/runAll/run.py
@@ -1,6 +1,6 @@
 import subprocess,time,sys
 
-TIME = 10                        #程序状态检测间隔（单位：分钟）
+TIME = 1                        #程序状态检测间隔（单位：分钟）
 # CMD = "/home/r201/Documents/PYTHON/main/Order_record/log.py"#需要执行程序的绝对路径，支持jar 如：D:\\calc.exe 或者D:\\test.jar
 chassis_movement ="/home/r201/Documents/PYTHON/main/chassis_movement/MQTT.py"
 log="/home/r201/Documents/PYTHON/main/Order_record/log.py"
