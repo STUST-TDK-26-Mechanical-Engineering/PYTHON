8bb44b20f90f96cea0ea9f2a1b96015ed1f547cf
LWCJacky <54478652+LWCJacky@users.noreply.github.com>
2022-10-10 23:50:21 +0800

    Update gpio_test.py

--- main/gpio/gpio_test.py
+++ main/gpio/gpio_test.py
@@ -30,33 +30,36 @@ def go(client: mqtt_client,GPIO,led1,led2):
     # client.on_message = on_message
 
 def run():
-    client = connect_mqtt()
-    GPIO.setmode(GPIO.BOARD)
-    mode = GPIO.getmode()
-    print(mode)
-    channel=32
-    a1=29
-    a2=31
-    led1=15
-    led2=33
-    GPIO.setup(a1, GPIO.IN)
-    GPIO.setup(led1, GPIO.OUT,initial=GPIO.HIGH)
-    GPIO.setup(led2, GPIO.OUT,initial=GPIO.HIGH)
-    time.sleep(1)
-    GPIO.setup(channel, GPIO.IN)
-    GPIO.setup(a2, GPIO.IN)
-    # client.loop_start()
-    while 1:
-        print(GPIO.input(channel),GPIO.input(a1),GPIO.input(a2))
-        GPIO.output(led1, GPIO.HIGH)
-        GPIO.output(led2, GPIO.LOW)
-        if GPIO.input(channel):
-            go(client,GPIO,led1,led2)
-            time.sleep(5)
+    try:
 
-        client.loop_start()    
-    # subscribe(client)
-    
+        client = connect_mqtt()
+        GPIO.setmode(GPIO.BOARD)
+        mode = GPIO.getmode()
+        print(mode)
+        channel=32
+        a1=29
+        a2=31
+        led1=15
+        led2=33
+        GPIO.setup(a1, GPIO.IN)
+        GPIO.setup(led1, GPIO.OUT,initial=GPIO.HIGH)
+        GPIO.setup(led2, GPIO.OUT,initial=GPIO.HIGH)
+        time.sleep(1)
+        GPIO.setup(channel, GPIO.IN)
+        GPIO.setup(a2, GPIO.IN)
+        # client.loop_start()
+        while 1:
+            print(GPIO.input(channel),GPIO.input(a1),GPIO.input(a2))
+            GPIO.output(led1, GPIO.HIGH)
+            GPIO.output(led2, GPIO.LOW)
+            if GPIO.input(channel):
+                go(client,GPIO,led1,led2)
+                time.sleep(5)
+
+            client.loop_start()    
+        # subscribe(client)
+    except KeyboardInterrupt as e:
+        GPIO.cleanup()    
     
     # client.loop_forever()
 if __name__ == '__main__':
