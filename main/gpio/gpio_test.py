6a9b3dc94d1962e7b5025ee60fe4d190a2b5ee6b
LWCJacky <54478652+LWCJacky@users.noreply.github.com>
2022-10-12 00:22:47 +0800

    Update gpio_test.py

--- main/gpio/gpio_test.py
+++ main/gpio/gpio_test.py
@@ -59,7 +59,7 @@ def run():
             if GPIO.input(channel):
                 go(client,GPIO,led1,led2)
                 time.sleep(5)
-                os._exit(0)
+                # os._exit(0)
 
             client.loop_start()    
         # subscribe(client)
